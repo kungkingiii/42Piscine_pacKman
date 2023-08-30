d3100b996fcbd88c87716fa9b0208e4a9790f338
unknown <chanisaraaah@gmail.com>
2023-08-31 00:01:34 +0700

    add memfn

---
+++ cadet/Rank0/Libft/ft_memcmp.c
@@ -0,0 +1,30 @@
+#include<stdlib.h>
+void *ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while(i < len)
+	{
+		if (((unsigned char *)s1)[i] !=  ((unsigned char *)s2)[i])
+		{
+			return ((void *)s1)[i] -  ((void *)s2)[i]
+		{
+		i++;
+	}
+	return 0;
+}
+
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char myarr[5];
+	char fnarr[5];
+	int myint[6];
+	int fnint[6];
+
+	printf("myfn: %s\n", myarr);
+	printf("orgfn: %s\n", myarr);
+	
+}
