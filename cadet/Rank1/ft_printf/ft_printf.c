1028cb637a897c60efb7c59ef235859436b2f36d
Chanisara Hongseng <chongsen@z2t5c1.42bangkok.com>
2023-10-27 17:37:50 +0700

    print %c

--- cadet/Rank1/ft_printf/ft_printf.c
+++ cadet/Rank1/ft_printf/ft_printf.c
@@ -1,21 +1,57 @@
-# include <stdlib.h>
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdint.h>
-# include <stdio.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chongsen <chongsen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/27 12:55:15 by chongsen          #+#    #+#             */
+/*   Updated: 2023/10/27 17:35:43 by chongsen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_printf(const char *str, ...)
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int ft_putchar(const char c)
 {
-	// int len;
+	return write (1, &c, 1); 
+}
+
+int ft_check_form(const char *str, va_list arg)
+{
+	if (*str == 'c')
+	{
+		return(ft_putchar (va_arg(arg, int)));
+	}
+	return (0);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		res;
 	va_list	arg;
 
 	va_start(arg, str);
-	printf("str is %s",str,&arg);
-	return(0);
+	// n = va_arg(arg, int);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			res = ft_check_form(str, arg);
+		}
+		str++;
+	}
+	va_end(arg);
+	return (res);
 }
 
-int main(void)
+int	main(void)
 {
-	ft_printf("abcde","abc");
-	printf("ss%s","abc");
+	ft_printf("abcde%c\n", 'a');
+	printf("ss%c", 'g');
 }
