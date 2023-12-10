072a8461414f72c463e7c914f45b3f3c01628064
unknown <chanisaraaah@gmail.com>
2023-12-10 14:04:54 +0700

    PUT STR

--- cadet/Rank1/ft_printf/ft_printf.c
+++ cadet/Rank1/ft_printf/ft_printf.c
@@ -18,23 +18,49 @@
 
 int ft_putchar(const char c)
 {
-	return write (1, &c, 1); 
+	return write(1, &c, 1);
+}
+
+int ft_putstr(const char *str)
+{
+	size_t i;
+
+	if (!str)
+	{
+		if (write(1, "(null)", 6) == -1)
+			return (-1);
+		return (6);
+	}
+	i = 0;
+	while (str[i])
+	{
+		if (write(1, &str[i], 1) == -1)
+			return (-1);
+		i++;
+	}
+	return (i);
 }
 
 int ft_check_form(const char *str, va_list arg)
 {
 	if (*str == 'c')
 	{
-		return(ft_putchar (va_arg(arg, int)));
+		return (ft_putchar(va_arg(arg, int)));
+	}
+	else if (*str == 's')
+	{
+		return (ft_putstr(va_arg(arg, char *)));
 	}
 	return (0);
 }
 
-int	ft_printf(const char *str, ...)
+int ft_printf(const char *str, ...)
 {
-	int		res;
-	va_list	arg;
+	int res;
+	int count;
+	va_list arg;
 
+	count = 0;
 	va_start(arg, str);
 	// n = va_arg(arg, int);
 	while (*str)
@@ -44,14 +70,19 @@ int	ft_printf(const char *str, ...)
 			str++;
 			res = ft_check_form(str, arg);
 		}
+		else
+		{
+			ft_putchar(*str);
+		}
 		str++;
+		count += res;
 	}
 	va_end(arg);
 	return (res);
 }
 
-int	main(void)
+int main(void)
 {
-	ft_printf("abcde%c\n", 'a');
-	printf("ss%c", 'g');
+	ft_printf("abcde%shh%c\n", "mytest", 'v');
+	printf("ss%svvl%c", "origin", 'v');
 }
