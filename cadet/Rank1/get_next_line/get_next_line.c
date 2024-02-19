794ba0eb09452349806c4053a884839c16775dda
unknown <chanisaraaah@gmail.com>
2024-02-19 23:52:48 +0700

    add gnl

--- cadet/Rank1/get_next_line/get_next_line.c
+++ cadet/Rank1/get_next_line/get_next_line.c
@@ -13,6 +13,42 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+typedef struct s_da
+{
+    char *s;
+    char *bf;
+    char *nl;
+    int read_f;
+    int error_malloc;
+    int error_join;
+} t_dat;
+
+char *get_next_line(int fd)
+{
+    static t_dat data;
+    // char *res;
+
+    data.s = "gg";
+
+    printf("%s", data);
+    printf("%d", fd);
+
+    // if (fd < 0 || BUFFER_SIZE < 1)
+    // 	return (NULL);
+    // data.bf = (char *)malloc(BUFFER_SIZE + 1);
+    // if (!data.bf)
+    // 	data.error_malloc = 1;
+    // if (!data.nl && !data.error_malloc)
+    // {
+    // 	if (!gnl2_read_dup(&data, fd))
+    // 		return (NULL);
+    // }
+    // free(data.bf);
+    // res = gnl4_result1(&data);
+    // return (res);
+    return "aaa";
+}
+
 int main(void)
 {
     int fd;
@@ -22,14 +58,14 @@ int main(void)
     s = get_next_line(fd);
     //	while (s)
     //	{
-    printf("%s", s);
-    free(s);
+    // printf("%s", s);
+    // free(s);
     //		s = get_next_line(fd);
     //	}
     //	free(s);
 
-    s = get_next_line(fd);
-    printf("%s", s);
-    free(s);
+    // s = get_next_line(fd);
+    // printf("%s", s);
+    // free(s);
     return (0);
 }
