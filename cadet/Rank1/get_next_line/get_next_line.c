60c726243934edc79630c2774833874d8539b650
unknown <chanisaraaah@gmail.com>
2024-02-22 00:36:02 +0700

    add gnl

--- cadet/Rank1/get_next_line/get_next_line.c
+++ cadet/Rank1/get_next_line/get_next_line.c
@@ -15,57 +15,39 @@
 
 typedef struct s_da
 {
-    char *s;
-    char *bf;
-    char *nl;
-    int read_f;
-    int error_malloc;
-    int error_join;
+	char *s;
+	char *bf;
+	char *nl;
+	int read_f;
+	int error_malloc;
+	int error_join;
 } t_dat;
 
 char *get_next_line(int fd)
 {
-    static t_dat data;
-    // char *res;
+	static t_dat data;
 
-    data.s = "gg";
-
-    printf("%s", data);
-    printf("%d", fd);
-
-    // if (fd < 0 || BUFFER_SIZE < 1)
-    // 	return (NULL);
-    // data.bf = (char *)malloc(BUFFER_SIZE + 1);
-    // if (!data.bf)
-    // 	data.error_malloc = 1;
-    // if (!data.nl && !data.error_malloc)
-    // {
-    // 	if (!gnl2_read_dup(&data, fd))
-    // 		return (NULL);
-    // }
-    // free(data.bf);
-    // res = gnl4_result1(&data);
-    // return (res);
-    return "aaa";
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
 }
 
 int main(void)
 {
-    int fd;
-    char *s;
+	int fd;
+	char *s;
 
-    fd = open("mytext.txt", O_RDONLY);
-    s = get_next_line(fd);
-    //	while (s)
-    //	{
-    // printf("%s", s);
-    // free(s);
-    //		s = get_next_line(fd);
-    //	}
-    //	free(s);
+	fd = open("mytext.txt", O_RDONLY);
+	s = get_next_line(fd);
+	//	while (s)
+	//	{
+	// printf("%s", s);
+	// free(s);
+	//		s = get_next_line(fd);
+	//	}
+	//	free(s);
 
-    // s = get_next_line(fd);
-    // printf("%s", s);
-    // free(s);
-    return (0);
+	// s = get_next_line(fd);
+	// printf("%s", s);
+	// free(s);
+	return (0);
 }
