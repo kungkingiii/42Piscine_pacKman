3b3c4a3f2c7110448b9f401a348ac135b8d4d8e6
Chanisara Hongseng <chongsen@z2t3c1.42bangkok.com>
2024-02-25 16:07:15 +0700

    add new code

--- cadet/Rank1/get_next_line/get_next_line.c
+++ cadet/Rank1/get_next_line/get_next_line.c
@@ -45,6 +45,148 @@ size_t ft_strlen(const char *s)
 // 	bytes = read(fd, text, BUFFER_SIZE);
 // }
 
+void	ft_strcat(char *dest, const char *src)
+{
+	// printf("ggg%sdfs%s\n", dest, src);
+	size_t	i;
+	size_t	l1;
+	size_t	max;
+
+	l1 = ft_strlen(dest);
+	max = ft_strlen(src);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[l1] = src[i];
+		l1++;
+		i++;
+	}
+	dest[l1] = '\0';
+}
+
+char	*check_next_line(char *text, int bytes, int fd)
+{
+	int		i;
+	char	*line_text;
+
+	bytes = read(fd, text, BUFFER_SIZE);
+	line_text = (char *)malloc(sizeof(char) * ft_strlen(text));
+	i = 0;
+	while (text[i])
+	{
+		// printf("stringgg cc:%c rr%c\n", line_text[i], text[i]);
+		if (text[i] == '\n')
+		{
+			// printf("stringgg :%c\n", text[i]);
+			line_text[i] = '\n';
+			// printf("stringgg ee:%s\n", line_text);
+			return (line_text);
+		}
+		line_text[i] = text[i];
+		// printf("stringgg aa:%s gg%c\n", line_text, text[i]);
+		i++;
+	}
+	// printf("stringgg aa:%c\n", line_text, text[i]);
+	return (line_text);
+}
+
+char *check_text(char *text, int bytes, int fd ,char *read_text)
+{
+	char *result;
+	int len;
+
+	result = read_text;
+	read_text = check_next_line(text, bytes, fd);
+				printf("read_text jaa :%s\n" , read_text);
+	ft_strcat(result, read_text);
+	len = ft_strlen(read_text);	
+	if (read_text[len] != '\0')
+	{
+		printf("resultttts :%s\n", result);
+		result = check_text(text,bytes,fd ,result);
+	}
+	return result;
+}
+
+char *get_next_line(int fd)
+{
+	// static t_dat data;
+	static char *text;
+	char *read_text;
+	char *result;
+	int bytes;
+	int len;
+	// static char buffer[5];
+	// ssize_t bytes_read = read(fd, buffer, BUFFER_SIZE);
+	// if (bytes_read == -1)
+	// {
+	// 	perror("read");
+	// 	close(fd);
+	// 	return buffer;
+	// }
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	text = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+
+	read_text = check_next_line(text, bytes, fd);
+
+	len = ft_strlen(read_text);
+
+	// // ft_strcat(result, read_text);
+
+//  read_text = check_next_line(text, bytes, fd);
+	result = read_text;
+	// // printf("result jaa :%s\n", result);
+
+	if (read_text[len] != '\n')
+	 {/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/18 17:35:43 by marvin            #+#    #+#             */
+/*   Updated: 2024/02/18 17:35:43 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 3
+
+typedef struct s_da
+{
+	char *s;
+	char *bf;
+	char *nl;
+	int read_f;
+	int error_malloc;
+	int error_join;
+} t_dat;
+
+size_t ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+// char *read_line(char *text, int bytes, int fd)
+// {
+// 	bytes = read(fd, text, BUFFER_SIZE);
+// }
+
 void ft_strcat(char *dest, const char *src)
 {
 	printf("ggg%sdfs%s\n", dest, src);
@@ -151,6 +293,48 @@ int main(void)
 	// printf("%s\n", s);
 	// s = get_next_line(fd);
 	// printf("%s\n", s);
+	//	while (s)
+	//	{
+	// printf("%s", s);
+	// free(s);
+	//		s = get_next_line(fd);
+	//	}
+	//	free(s);
+
+	// s = get_next_line(fd);
+	// printf("%s", s);
+	// free(s);
+	return (0);
+}
+	// 	read_text = check_next_line(text, bytes, fd);
+	// 			// printf("read_text jaa :%s\n" , read_text);
+	// 	// printf(" result naaa: %s\n ", result);
+
+
+		// printf("resultttts :%s\n", result);
+	// 	ft_strcat(result, read_text);
+			result = check_text(text,bytes,fd ,read_text);
+	 }
+
+	return result;
+}
+
+int main(void)
+{
+	int fd;
+	char *s;
+
+	fd = open("mytext.txt", O_RDONLY);
+	s = get_next_line(fd);
+	printf("%s\n", s);
+	s = get_next_line(fd);
+	printf("%s\n", s);
+	s = get_next_line(fd);
+	printf("%s\n", s);
+	s = get_next_line(fd);
+	printf("%s\n", s);
+		s = get_next_line(fd);
+	printf("%s\n", s);
 	//	while (s)
 	//	{
 	// printf("%s", s);
