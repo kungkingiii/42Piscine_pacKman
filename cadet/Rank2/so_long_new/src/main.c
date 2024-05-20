76f250f7f47a203742205bf462db7e2a7ba020df
pack <chanisaraaaaaiii@gmail.com>
2024-05-20 21:35:00 +0700

    still error

--- cadet/Rank2/so_long_new/src/main.c
+++ cadet/Rank2/so_long_new/src/main.c
@@ -6,13 +6,11 @@
 /*   By: packmanich <packmanich@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 20:39:23 by packmanich        #+#    #+#             */
-/*   Updated: 2024/05/20 01:02:06 by packmanich       ###   ########.fr       */
+/*   Updated: 2024/05/20 01:52:10 by packmanich       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../so_long.h"
-#define    IM1_SX        42
-#define    IM1_SY        42
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -50,10 +48,10 @@ int	main()
 	data.mlx = mlx_init();
 	data.window = mlx_new_window(data.mlx, 640, 640, "so_long");
 
-	data.img2 = mlx_xpm_file_to_image(data.mlx,"../textures/player.xpm",&a,&b);
-	mlx_put_image_to_window(data.mlx,data.window,data.img2,1,1);
-	data.img1 = mlx_xpm_file_to_image(data.mlx,"../textures/enemy.xpm",&a,&b);
-	mlx_put_image_to_window(data.mlx,data.window,data.img1,64,64);
+	data.img2 = mlx_xpm_file_to_image(data.mlx,"open.xpm",&a,&b);
+	mlx_put_image_to_window(data.mlx,data.window,data.img2,10,10);
+	// data.img1 = mlx_xpm_file_to_image(data.mlx,"../textures/enemy.xpm",&a,&b);
+	// mlx_put_image_to_window(data.mlx,data.window,data.img1,64,64);
 	mlx_key_hook(data.window, key_hook, &data);
 	mlx_loop(data.mlx);
  
